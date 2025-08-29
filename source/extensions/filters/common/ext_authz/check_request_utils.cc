b36bb60ee20258a511164672e03146e2d48ee13d
Matthew McLeod <mail@matthewmcleod.co.uk>
2025-08-29 13:28:31 +0100

    fix: resolve compilation errors in ext_authz check_request_utils
    - Replace ProtobufWkt::Struct with Protobuf::Struct
    - Fix type conversion by using SerializeAsString() instead of MessageUtil::hash()
    
    This fixes the compilation errors that were causing CI failures:
    - use of undeclared identifier 'ProtobufWkt'
    - no viable conversion from 'std::size_t' to 'std::string'
    
    Signed-off-by: Matthew McLeod <mail@matthewmcleod.co.uk>

--- source/extensions/filters/common/ext_authz/check_request_utils.cc
+++ source/extensions/filters/common/ext_authz/check_request_utils.cc
@@ -363,7 +363,7 @@ CheckRequestUtils::computePeerMetadataHeaders(const StreamInfo::StreamInfo& stre
 
   // Create metadata as base64-encoded protobuf Struct
   // Prefer full node metadata; fall back to minimal if empty
-  ProtobufWkt::Struct metadata_struct;
+  Protobuf::Struct metadata_struct;
   if (local_info.node().has_metadata()) {
     // Use the full node metadata if available
     metadata_struct = local_info.node().metadata();
@@ -379,19 +379,19 @@ CheckRequestUtils::computePeerMetadataHeaders(const StreamInfo::StreamInfo& stre
     (*metadata_struct.mutable_fields())["PRINCIPAL"].set_string_value(principal);
   }
 
-  std::string serialized = MessageUtil::hash(metadata_struct);
+  std::string serialized = metadata_struct.SerializeAsString();
 
   // Add size cap to prevent header bloat
   constexpr size_t kMaxMetadataSize = 8192; // 8KB limit
   if (serialized.size() > kMaxMetadataSize) {
     ENVOY_LOG_MISC(warn, "Peer metadata size {} exceeds limit {}, truncating", serialized.size(),
                    kMaxMetadataSize);
-    ProtobufWkt::Struct truncated_struct;
+    Protobuf::Struct truncated_struct;
     (*truncated_struct.mutable_fields())["WORKLOAD_NAME"].set_string_value(peer_id);
     if (!principal.empty()) {
       (*truncated_struct.mutable_fields())["PRINCIPAL"].set_string_value(principal);
     }
-    serialized = MessageUtil::hash(truncated_struct);
+    serialized = truncated_struct.SerializeAsString();
   }
 
   const std::string b64 = Envoy::Base64::encode(serialized.c_str(), serialized.size());
